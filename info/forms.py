82cc444ebd4abb564bea47915312eb6dddc7194c
Nirmit73 <nirmitpatel73@gmail.com>
2024-11-01 21:28:34 -0400

    Updated forms to include email field

--- info/forms.py
+++ info/forms.py
@@ -1,6 +1,23 @@
 from django.forms import ModelForm
 from .models import Comment
 
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Required. Enter a valid email address.")
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(SignupForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 class CommentForm(ModelForm):
     class Meta:
