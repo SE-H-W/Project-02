8151c6d36e46d669f5d434a944747aff2f6e2390
Nirmit73 <nirmitpatel73@gmail.com>
2024-11-01 21:30:46 -0400

    Added url patterns for password reset

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from .views import city_suggestions, city_photo
 
 urlpatterns = [
     path("city", city_suggestions, name="city_search"),
     path("city/photo", city_photo, name="city_photo"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
